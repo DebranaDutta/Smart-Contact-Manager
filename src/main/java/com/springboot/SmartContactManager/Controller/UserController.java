7384c51ad1ff9be6e8f8a8fabbca0ff706236692
DebranaDutta <debduttaother@gmail.com>
2023-07-01 16:35:29 +0530

    1st July 2023 2nd Commit

--- src/main/java/com/springboot/SmartContactManager/Controller/UserController.java
+++ src/main/java/com/springboot/SmartContactManager/Controller/UserController.java
@@ -1,14 +1,29 @@
 package com.springboot.SmartContactManager.Controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.SmartContactManager.Dao.UserRepository;
+import com.springboot.SmartContactManager.Model.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping("/index")
-	public String Dashboard() {
+	public String Dashboard(Model model, Principal principal) {
+		String userName = principal.getName();
+		// Get user using userName(Email)
+		User user = userRepository.getUserByUserName(userName);
+		model.addAttribute("user", user);
 		return "Normal/UserDashboard";
 	}
 }
